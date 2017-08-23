aa7fd44790095c6cb89ed7df2b45b53fa8dae618
Leydson Vieira <leydson.vieira@gmail.com>
2017-08-23 16:43:50 -0300

    Create decrypter.py

---
+++ decrypter.py
@@ -0,0 +1,24 @@
+# -*- encoding:utf-8 -*-
+import string
+
+class Decrypter(object):
+    '''
+    Utiliza da cifra de cesar para buscar padrões de decriptados de palavras
+    '''
+
+    def __init__(self, enc_word, enc_key):
+        self.enc_word = enc_word
+        self.enc_key = enc_key
+
+    def decrypt(self):
+        word = ''
+        for index, i in enumerate(self.enc_word):
+            word += string.ascii_uppercase[index + self.enc_key]
+        print word
+
+if __name__ == "__main__":
+    enc_word = input('Digite a frase encriptada: ')
+    enc_key = input('Digite o número da chave: ')
+    print '\n ---------- \n'
+    d = Decrypter(enc_word.upper(), int(enc_key))
+    d.decrypt()
